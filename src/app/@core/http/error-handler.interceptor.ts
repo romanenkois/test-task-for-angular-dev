b74da4a5ef0e8bfe7096a4d8468d3da64bb2163b
Romanenko Illya <romanenkoisnot@gmail.com>
2025-11-22 19:51:58 +0200

    step final

--- src/app/@core/http/error-handler.interceptor.ts
+++ src/app/@core/http/error-handler.interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { environment } from '@env/environment';
@@ -20,11 +20,20 @@ export class ErrorHandlerInterceptor implements HttpInterceptor {
   }
 
   // Customize the default error handler here if needed
-  private errorHandler(response: HttpEvent<any>): Observable<HttpEvent<any>> {
+  private errorHandler(response: HttpErrorResponse): Observable<HttpEvent<any>> {
     if (!environment.production) {
       // Do something with the error
       log.error('Request error', response);
     }
-    throw response;
+
+    // Handle 404 errors specifically
+    if (response.status === 404) {
+      log.error('Resource not found (404)', {
+        url: response.url,
+        message: response.message,
+      });
+    }
+
+    return throwError(response);
   }
 }
